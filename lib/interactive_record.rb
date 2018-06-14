09d441f9f37984267fc6ac764040e49d930d9968
Nicholas Powers <njpowers7915@gmail.com>
2018-06-14 17:02:54 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,7 +49,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute)
+  def self.find_by(attribute={})
     sql = "SELECT * FROM #{self.table_name} WHERE attribute = '#{attribute[value]}' LIMIT 1"
     DB[:conn].execute(sql)
   end
