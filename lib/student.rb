d85ab29ddb549959b9129ee42ae406d834fa8958
Nicholas Powers <njpowers7915@gmail.com>
2018-06-14 16:09:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,11 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   attr_accessor :name, :grade, :id
-  
+
   def initialize(id: nil, name: name, grade: grade)
     @id = id
     @name = name
     @grade = grade
   end
-  
+
 end
