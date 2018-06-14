7f4fbcc3b843f7cbd25b93db98a421f7c1e02808
Nicholas Powers <njpowers7915@gmail.com>
2018-06-14 17:57:57 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,15 @@
+
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  attr_accessor :name, :grade, :id
+
+  def initialize(id: nil, name: name, grade: grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
 end
